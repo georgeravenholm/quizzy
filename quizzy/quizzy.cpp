32dfb0fa22d0df47317002b083e8faf6f612c6b4
georgeravenholm <ravenholmgeorge@gmail.com>
2018-02-07 21:03:22 +1100

    successfully parsing text file into question and answer objects

--- quizzy/quizzy.cpp
+++ quizzy/quizzy.cpp
@@ -1,11 +1,97 @@
-// quizzy.cpp : Defines the entry point for the console application.
-//
-
+// QUIZZY!
+// georgeravenholm
 #include "stdafx.h"
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include <algorithm> // shuffle
+#include <random>
+
+struct Answer
+{
+	Answer() = default;
+	Answer(std::string txt, bool correct) { this->txt = txt; this->correct = correct; }
+	std::string txt = "";
+	bool correct = false;
+};
+
+struct Question
+{
+	Question(std::string txt) { this->txt = txt; }
+	void Shuffle()
+	{
+		std::random_device rd;
+		std::mt19937 g(rd());
+
+		std::shuffle(answers.begin(), answers.end(), g);
+	}
+	std::string txt;
+	std::vector<Answer> answers;
+};
 
 int main()
 {
+	// read file
+	std::ifstream file("sample.txt");
+	if (!file) {std::cerr << "file error"; return 1;}
+
+	// parse file
+	std::string str;
+	std::vector<Question> questions;
+
+	// check first line
+	std::getline(file, str);
+	if (str != "quiz:") { std::cerr << "invalid quiz file"; return 1; }
+
+	while (std::getline(file, str))
+	{
+		
+		if (str == "question:")
+		{
+			// new question
+			std::getline(file, str); // read next line, question text
+			Question current = Question(str);// gm_construct new question object
+			
+			while (std::getline(file, str) ) // scan for all answers
+			{
+				if (str == "correct:" || str == "wrong:")
+				{
+					bool correct = str == "correct:"; // see if this is a correct or wrong answer
+					// found an answer
+					std::getline(file, str); // read next line, answer text
+					current.answers.push_back( Answer(str , correct ) );	// gm_construct new answer object, generate bool if we are
+																					// doing correct: or wrong:
+				}
+				else if (str == "quizend:") goto heck;
+				else if (str == "null:") continue;
+				else if (str[0] == '#') continue; // allow for comments
+				else if (str[0] == '\n') continue; // skip newlines
+
+				else if (str == "questionend:") break; // get outa here!
+
+			}
+
+			// now we have generated the question fully, put it into vector
+			current.Shuffle(); // shuffle answers
+			questions.push_back(current);
+
+		}
+		else if (str == "quizend:") goto heck;
+		else if (str == "null:") continue;
+		else if (str[0] == '#') continue; // allow for comments
+		else if (str[0] == '\n') continue; // skip newlines
+
+		std::cout << str << std::endl;
+	}
+	
+	heck: 
+
+#ifdef _DEBUG
+	system("pause");
+#endif
     return 0;
 }
 
